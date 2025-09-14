4fd6e554c4f04fed2ccc64444183303048d297ad
mansii1 <mansimangukiya0@gmail.com>
2025-09-14 23:49:20 +0530

    Complted

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
 import "./style.css";
+
+const nav = document.querySelector(".nav") as HTMLDivElement;
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
